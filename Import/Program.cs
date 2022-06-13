3c13a383e79aa68fc49d7d7ed5e77033065aaa53
Jie Tian <jie.tian@innocellence.com>
2022-06-13 09:38:29 +0800

    change to sync model, it works in aws

--- Import/Program.cs
+++ Import/Program.cs
@@ -31,29 +31,29 @@ namespace Import
             bucketName = ConfigurationManager.AppSettings["bucketName"];
             dbName = ConfigurationManager.AppSettings["dbName"];
 
-            //#if DEBUG
+#if DEBUG
             string[] importList;
             using (StreamReader sr = new StreamReader("index.ls"))
             {
                 string str = sr.ReadToEnd();
                 importList = str.Split(new char[] { '\n', '\r' });
             }
-            //#else
+#else
 
-            //// 获取需要导出的表
-            //var s3Client = new AmazonS3Client(ak, sk, Amazon.RegionEndpoint.CNNorthWest1);
-            //TransferUtility fileTransferUtility = new TransferUtility(s3Client);
-            //Console.WriteLine("starting to get export table list from s3...");
-            //Console.WriteLine("key: {0}", $"datamigration/{dbName}/index.ls");
-            //fileTransferUtility.Download("index.ls", bucketName, $"datamigration/{dbName}/index.ls");
+            // 获取需要导出的表
+            var s3Client = new AmazonS3Client(ak, sk, Amazon.RegionEndpoint.CNNorthWest1);
+            TransferUtility fileTransferUtility = new TransferUtility(s3Client);
+            Console.WriteLine("starting to get export table list from s3...");
+            Console.WriteLine("key: {0}", $"datamigration/{dbName}/index.ls");
+            fileTransferUtility.Download("index.ls", bucketName, $"datamigration/{dbName}/index.ls");
 
-            //string[] importList;
-            //using (StreamReader sr = new StreamReader("index.ls"))
-            //{
-            //    string str = sr.ReadToEnd();
-            //    importList = str.Split(new char[] { '\n', '\r' });
-            //}
-            //#endif
+            string[] importList;
+            using (StreamReader sr = new StreamReader("index.ls"))
+            {
+                string str = sr.ReadToEnd();
+                importList = str.Split(new char[] { '\n', '\r' });
+            }
+#endif
 
             var assemblies = AppDomain.CurrentDomain.GetAssemblies();
             var modelAssembly = assemblies.FirstOrDefault(a => a.ManifestModule.Name == "Models.dll");
@@ -66,76 +66,7 @@ namespace Import
                 }
             }
 
-            //Task.WaitAll(tasks.ToArray());
         }
-
-        //public static async Task Import<T>() where T : IEntity, new()
-        //{
-        //    var tableName = typeof(T).Name;
-        //    Console.WriteLine($"Downloading {tableName}...");
-        //    System.Diagnostics.Stopwatch watch = new System.Diagnostics.Stopwatch();
-        //    // 将文件导入到PG数据库里
-        //    var connString = ConfigurationManager.ConnectionStrings[dbName].ConnectionString;
-        //    DbContextOptions<PostgreSQLContext> dbContextOption = new DbContextOptions<PostgreSQLContext>();
-        //    DbContextOptionsBuilder<PostgreSQLContext> dbContextOptionBuilder = new DbContextOptionsBuilder<PostgreSQLContext>(dbContextOption);
-        //    AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
-        //    var db = new PostgreSQLContext(dbContextOptionBuilder.UseNpgsql(connString).Options);
-
-        //    watch.Restart();
-
-        //    for (var i = 0; ; i++)
-        //    {
-        //        var key = $"datamigration/{dbName}/{tableName}/{tableName}.{i.ToString("D4")}.json";
-        //        Console.WriteLine($"Downloading {key}...");
-
-        //        var s3Client = new AmazonS3Client(ak, sk, region);
-
-        //        // 从S3上拉文件下来
-        //        lock (downloadLocker)
-        //        {
-
-        //            TransferUtility fileTransferUtility = new TransferUtility(s3Client);
-        //            try
-        //            {
-        //                fileTransferUtility.Download("files/" + key, bucketName, key);
-        //            }
-        //            catch(Exception ex)
-        //            {
-        //                if (ex.Message.Contains("The specified key does not exist"))
-        //                {
-        //                    break;
-        //                }
-        //                Console.WriteLine(ex.Message);
-        //                break;
-        //            }
-
-        //            Console.WriteLine($"downloaded file {key} to local folder: {watch.ElapsedMilliseconds / 1000.0}s");
-        //        }
-        //        // TODO: 读取json文件
-        //        List<T> result;
-        //        using (StreamReader sr = new StreamReader("files/" + key))
-        //        {
-        //            string json = sr.ReadToEnd();
-        //            result = Newtonsoft.Json.JsonConvert.DeserializeObject<List<T>>(json);
-        //        }
-        //        Console.WriteLine($"finished deserialize<{tableName}>: {watch.ElapsedMilliseconds / 1000.0}s, total {result.Count} records");
-        //        File.Delete("files/" + key);
-        //        Console.WriteLine($"start to import {tableName} to pg database...");
-
-
-        //        db.BulkInsert<T>(result, options => options.InsertKeepIdentity = true);
-        //        //db.AddRange(result);
-        //        //Console.WriteLine($"  data added to {tableName}.{i.ToString("D4")} to mssql database... - {watch.ElapsedMilliseconds / 1000.0}s");
-        //        //db.BulkSaveChanges();
-        //        Console.WriteLine($"  finished to import {tableName}.{i.ToString("D4")} to mssql database... - {watch.ElapsedMilliseconds / 1000.0}s");
-
-        //    }
-        //    await db.DisposeAsync();
-
-        //    watch.Stop();
-        //    Console.WriteLine($"finished import {tableName} to db: {watch.ElapsedMilliseconds / 1000.0}s");
-
-        //}
         public static void Import<T>() where T : IEntity, new()
         {
             var tableName = typeof(T).Name;
@@ -185,10 +116,11 @@ namespace Import
                     File.Delete("files/" + key);
                     Console.WriteLine($"  finished deserialize<{tableName}>: {watch.ElapsedMilliseconds / 1000.0}s, total {result.Count} records");
 
-                    tasks.Add(ImportToDB<T>(tableName, key, watch, db, i, result));
+                    var task = ImportToDB<T>(tableName, key, watch, db, i, result);
+                    task.Wait();
                 }
 
-                Task.WaitAll(tasks.ToArray());
+                //Task.WaitAll(tasks.ToArray());
                 db.Dispose();
 
                 Console.WriteLine($"finished import {tableName} to db: {watch.ElapsedMilliseconds / 1000.0}s");
@@ -219,7 +151,7 @@ namespace Import
             {
                 Console.WriteLine($"  start to import {tableName}.{page.ToString("D4")} to pg database... - {watch.ElapsedMilliseconds / 1000.0}s");
 
-                db.BulkInsert<T>(result, options => options.InsertKeepIdentity = true);
+                db.BulkInsert(result, options => options.InsertKeepIdentity = true);
                 //db.AddRange(result);
                 //Console.WriteLine($"  data added to {tableName}.{i.ToString("D4")} to mssql database... - {watch.ElapsedMilliseconds / 1000.0}s");
                 //db.BulkSaveChanges();
